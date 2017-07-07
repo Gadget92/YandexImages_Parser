720cb936c7d521f3149fc96d46114b220f34e9ba
V.Stefanovich <vladislav.stefanovich@softclub.by>
2017-07-07 17:59:10 +0300

    Movie code to methods,created the basis for a method of parsing the name of the image

--- main.py
+++ main.py
@@ -1,17 +1,25 @@
 import urllib.request
 import os
-save_path = "D:\\Wallpapers\\Yandex\\"
-screen_size = ["1920x1200", "1920x1080", "1280x1024"]
+
+SAVE_PATH = "D:\\Wallpapers\\Yandex\\"
+SCREEN_SIZE = ["1920x1200", "1920x1080", "1280x1024"]
 
 def saveTodayImage(ImageName):
-    for screen in screen_size:
+    for screen in SCREEN_SIZE:
         url = 'https://yandex.by/images/today?size=' + screen
-        if not os.path.exists(save_path+str(screen)):
-            os.makedirs(save_path+str(screen))
-        fullPath = os.path.join(save_path+str(screen), str(screen)+".jpg")
-        urllib.request.urlretrieve(url, fullPath)
+        if not os.path.exists(SAVE_PATH + str(screen)):
+            os.makedirs(SAVE_PATH + str(screen))
+        fullpath = os.path.join(SAVE_PATH + str(screen), str(screen) + ".jpg")
+        urllib.request.urlretrieve(url, fullpath)
 
 def GetTodayImageName():
+    NAME_URL = "https://yandex.by/images/"
+    # Need add parse image name
     TodayImageName = ""
 
     return TodayImageName
+
+
+ImageName = GetTodayImageName()
+if ImageName != "":
+    saveTodayImage(ImageName)
