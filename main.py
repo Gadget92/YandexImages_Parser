21e5b4c29750010b2be67ff116dfa897a965cc10
V.Stefanovich <vladislav.stefanovich@softclub.by>
2017-08-23 17:47:13 +0300

    Little fix code

--- main.py
+++ main.py
@@ -2,8 +2,10 @@ import urllib.request
 import platform
 import requests
 import os
+import configparser
+# from lxml import html
+
 from bs4 import BeautifulSoup
-from lxml import html
 
 SAVE_PATH_WIN = "D:\\Wallpapers\\Yandex\\"
 SAVE_PATH_NIX = "/home/gadget/Изображения/"
@@ -12,22 +14,17 @@ SCREEN_SIZE = ["1920x1200", "1920x1080", "1280x1024"]
 def saveTodayImage(ImageName, save_path):
     for screen in SCREEN_SIZE:
         url = 'https://yandex.by/images/today?size=' + screen
-
         if not os.path.exists(save_path + str(screen)):
             os.makedirs(save_path + str(screen))
         fullpath = os.path.join(save_path + str(screen), ImageName + ".jpg")
         urllib.request.urlretrieve(url, fullpath)
-        print('Image succesful saved')
+        print('Image succesful saved, size: ' + str(screen))
 
 def GetTodayImageName():
     NAME_URL = "https://yandex.by/images/"
     r = requests.get(NAME_URL)
-    # Save html to file
-    # with open('test.html', 'w', encoding='utf-8') as output:
-    #     output.write(r.text)
-    soup = BeautifulSoup(r.text)
+    soup = BeautifulSoup(r.text, "html.parser")
     TodayImageName = soup.find('span', {'class': 'b-501px__name'}).text
-
     return TodayImageName
 
 
